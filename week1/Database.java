1fa2ae0663c555dbf53baec1d898324ab0926d0b
Nishant Sinha <nishnha@gmail.com>
2016-09-15 11:10:40 -0400

    added week1

---
+++ week1/Database.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Database {
+
+  private Map<String,Relation> relations;
+
+  // METHODS
+
+  // Constructor; creates the empty HashMap object
+  public Database() {
+        relations = new HashMap<String, Relation>();
+  }
+
+  // Add relation r with name rname to HashMap
+  // if relation does not already exists.
+  // Make sure to set the name within r to rname.
+  // return true on successful add; false otherwise
+  public boolean addRelation(String rname, Relation r) {
+        if (relations.containsKey(rname) == true || relations.containsValue(r) == true)
+            return false;
+        relations.put(rname, r);
+        return true;
+  }
+
+  // Delete relation with name rname from HashMap
+  // if relation exists. return true on successful delete; false otherwise
+  public boolean deleteRelation(String rname) {
+        if (relations.containsKey(rname)) {
+            relations.remove(rname);
+            return true;
+        }
+        return false;
+  }
+
+  // Return true if relation with name rname exists in HashMap
+  // false otherwise
+  public boolean relationExists(String rname) {
+        if (relations.containsKey(rname))
+            return true;
+        return false;
+  }
+
+  // Retrieve and return relation with name rname from HashMap;
+  // return null if it does not exist.
+  public Relation getRelation (String rname) {
+        if (relationExists(rname))
+            return relations.get(rname);
+        return null;
+  }
+
+  // Print database schema to screen.
+  public void displaySchema() {
+        for (Relation r : relations.values())
+            System.out.println(r);
+  }
+
+}
---
+++ week4/Tuple.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.Arrays;
+
+public class Tuple {
+
+    private ArrayList<String> attributes;
+    private ArrayList<String> domains;
+    private ArrayList<Comparable> tuple;
+
+    // METHODS
+
+    // Constructor; set instance variables
+    public Tuple (ArrayList<String> attr, ArrayList<String> dom) {
+        attributes = new ArrayList<String>(attr);
+        domains = new ArrayList<String>(dom);
+        tuple = new ArrayList<Comparable>();
+    }
+
+    // Return true if this tuple is equal to compareTuple
+    public boolean equals (Tuple compareTuple) {
+        if (this.tuple.equals(compareTuple.tuple))
+            return true;
+        return false;
+    }
+
+    // Add String s at the end of the tuple
+    public void addStringComponent(String s) {
+        tuple.add(s);
+    }
+
+    // Add Double d at the end of the tuple
+    public void addDoubleComponent(Double d) {
+        tuple.add(d);
+    }
+
+    // Add Integer i at the end of the tuple
+    public void addIntegerComponent(Integer i) {
+        tuple.add(i);
+    }
+
+    // return String representation of tuple; See output of run for format.
+    public String toString() {
+        String output = new String();
+        for (Comparable c : tuple) {
+            output = output + c + ":";
+        }
+        return output;
+    }
+
+}
