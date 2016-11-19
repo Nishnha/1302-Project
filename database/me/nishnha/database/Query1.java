5e63b52d67d79c906c084c23d7cadec41e556114
Nishant Sinha <nishnha@gmail.com>
2016-11-19 17:35:35 -0500

    imported into ide

--- database/me/nishnha/database/Query1.java
+++ database/me/nishnha/database/Query1.java
@@ -1,7 +1,25 @@
 package me.nishnha.database;
 
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+
 /**
  * Created by nishnha on 11/13/16.
  */
 public class Query1 {
+    public static void main(String[] args) {
+        Database db = new Database();
+        db.initializeDatabase(args[0]);
+        Relation r1 = db.getRelation("EMPLOYEE");
+        Relation r2 = db.getRelation("DEPARTMENT");
+
+        ArrayList<String> cnames = new ArrayList<String>() {{
+            add("FNAME");
+            add("LNAME");
+            add("ADDRESS");
+        }};
+        Relation r3 = r2.project(cnames);
+        System.out.println(r1);
+        System.out.println(r3);
+    }
 }
--- database/me/nishnha/database/Query3.java
+++ database/me/nishnha/database/Query3.java
@@ -4,4 +4,11 @@ package me.nishnha.database;
  * Created by nishnha on 11/17/16.
  */
 public class Query3 {
+    public static void main(String[] args) {
+        Database db = new Database();
+        db.initializeDatabase(args[0]);
+        Relation r1 = db.getRelation("EMPLOYEE");
+        Relation r2 = db.getRelation("DEPARTMENT");
+
+    }
 }
